236e59d5791bdaeba7e7bc02171bf66c88ce8526
Jeremy Heminger <webdev@Jeremys-Mac-mini.local>
2019-02-01 12:36:53 -0800

    bugfix sidebar

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
 add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
-	// parent css
+    // parent css
     $parent_style = 'parent-style'; 
     //wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     // child css
@@ -12,10 +12,10 @@ function my_theme_enqueue_styles() {
     );
     // js
     wp_enqueue_script( 'child-script', 
-    	get_stylesheet_directory_uri() . '/js/script.js', 
-    	array( 'jquery' ),
-    	wp_get_theme()->get('Version'),
-    	true 
+        get_stylesheet_directory_uri() . '/js/script.js', 
+        array( 'jquery' ),
+        wp_get_theme()->get('Version'),
+        true 
     );
 }
 
@@ -23,7 +23,7 @@ add_action( 'widgets_init', 'my_theme_widgets_init' );
 function my_theme_widgets_init() {
     register_sidebar( array(
         'name'          => __( 'Blog Sidebar', 'twentynineteen-child' ),
-        'id'            => 'sidebar-1',
+        'id'            => 'sidebar-2',
         'description'   => __( 'Add widgets here to appear in your sidebar on blog posts and archive pages.', 'twentynineteen-child' ),
         'before_widget' => '<section id="%1$s" class="widget %2$s">',
         'after_widget'  => '</section>',
