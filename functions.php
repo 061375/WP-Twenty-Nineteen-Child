cc1ba86a42accd1c8afbc81c4734d69c34e5360e
Jeremy Heminger <webdev@Jeremys-Mac-mini.local>
2019-02-01 10:44:30 -0800

    css grids

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ add_action( 'wp_enqueue_scripts', 'my_theme_enqueue_styles' );
 function my_theme_enqueue_styles() {
 	// parent css
     $parent_style = 'parent-style'; 
-    wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
+    //wp_enqueue_style( $parent_style, get_template_directory_uri() . '/style.css' );
     // child css
     wp_enqueue_style( 'child-style',
         get_stylesheet_directory_uri() . '/style.css',
@@ -17,6 +17,19 @@ function my_theme_enqueue_styles() {
     	wp_get_theme()->get('Version'),
     	true 
     );
+}
+
+add_action( 'widgets_init', 'my_theme_widgets_init' );
+function my_theme_widgets_init() {
+    register_sidebar( array(
+        'name'          => __( 'Blog Sidebar', 'twentynineteen-child' ),
+        'id'            => 'sidebar-1',
+        'description'   => __( 'Add widgets here to appear in your sidebar on blog posts and archive pages.', 'twentynineteen-child' ),
+        'before_widget' => '<section id="%1$s" class="widget %2$s">',
+        'after_widget'  => '</section>',
+        'before_title'  => '<h2 class="widget-title">',
+        'after_title'   => '</h2>',
+    ) );
 }
 
 
